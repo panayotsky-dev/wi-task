9e9ed1fa26e68cafc0229612ea899bd082671f65
Panayot Petkov <panayotsky.dev@gmail.com>
2023-11-14 22:47:17 +0200

    Header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -33,22 +33,65 @@ function Header() {
  ]
 
   return (
-    <div className='w-full bg-white text-black h-[180px] px-[40px]'>
-       <div className='mt-[19px] justify-between text-black flex'>
+    <div className='w-full bg-white text-black  h-full '>
+       <div className='px-[40px] mt-[19px] justify-between text-black flex'>
         <div className='flex flex-row items-center justify-center '>
             <img src='logo.png' alt='logoImage' className='mr-[16px] mt-1 w-[22px] h-[24px]'/>
             <h1 className=' text-[24px] font-sans ' >Clay Shop</h1>
         </div>
         <div className='grid  grid-cols-3 gap-x-[40px] items-center'>
+        <img src='/Icons/instagram_icn _1.png' />
+        <img src='/Icons/twitter_icn_1.png' />
             <img src='/Icons/facebook_icn_1.png' />
-            <img src='/Icons/instagram_icn _1.png' />
-            <img src='/Icons/twitter_icn_1.png' />
+            
+            
         </div>
         <div className='grid grid-cols-2 gap-[24px] justify-between items-center'>
             <HiMagnifyingGlass />
             <IoBagOutline />
         </div>
        </div>
+       <div className=' justify-center grid items-center mt-[68px] '> 
+       <div className='  justify-center grid grid-cols-4 place-items-center items-center gap-x-[40px]  '>
+        <div>
+        <h1 className='text-[24px] font-sans'>
+            Home
+        </h1>
+        <h1 className='text-[24px] font-sans flex justify-center'>
+            |
+        </h1>
+        </div>
+        <div>
+        <h1 className='text-[24px] font-sans'>
+            Shop
+        </h1>
+        <h1 className='text-[24px] font-sans flex justify-center invisible'>
+            |
+        </h1>
+        </div>
+        <div>
+        <h1 className='text-[24px] font-sans'>
+            Blog
+        </h1>
+        <h1 className='text-[24px] font-sans flex justify-center invisible'>
+            |
+        </h1>
+        </div>
+        <div className=''>
+        <h1 className='text-[24px] font-sans'>
+            Contact
+        </h1>
+        <h1 className='text-[24px] font-sans flex justify-center invisible '>
+            |
+        </h1>
+        </div>
+        
+       
+        
+       </div>
+
+       </div>
+       
 
     </div>
   )
