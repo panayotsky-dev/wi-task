27a7947265c836ded1bb1b70a44a514cee42e92c
Panayotsky-dev <panayotsky.dev@gmail.com>
2023-11-15 23:32:46 +0200

    +-

--- src/app/components/CardComponent.jsx
+++ src/app/components/CardComponent.jsx
@@ -3,27 +3,38 @@ import React from 'react'
 function CardComponent() {
   return (
   
-  <div className=' w-[1100px] justify-center  flex bg-white mt-[62px] text-black   max-h-[660px]'>
-    <div className='grid grid-cols-2'>
-        <div className=' flex  flex-row justify-center bg-slate-500'>
+  <div className=' w-[1100px] justify-center  flex bg-white mt-[62px] text-black     max-h-[660px] rounded-xl'>
+    <div className='grid grid-cols-2 '>
+        <div className=' flex  flex-col justify-center pl-[40px]  pb-[40px] '>
             <div className='mt-[40px] px-[156px] justify-center items-center flex-col flex'>
                 <img src='/product/dres1.png' alt='dres1' className=' object-cover max-w-[280px] max-h-[436px]' />
-                <div className='grid grid-cols-5  mt-[56px] bg-purple-400'>
-                    <div className='w-[88px] h-[88px]  bg-purple-400  border-2 rounded-sm justify-center items-center  '>
-                        <img src='/product/dres1.png' className='w-[46px] h-[72px] ' />
+                <div className='flex flex-row mt-[56px]  '>
+                   
+                    <div className='w-[88px] h-[88px]  rounded-md flex border-2  justify-center items-center  mr-[24px]'>
+                        <img src='/product/dres1.png' className='w-[46px] h-[72px]  hover:scale-105 ' />
                     </div>
-                    <div className='w-[88px] h-[88px]  bg-purple-400 flex border-2 rounded-sm justify-center items-center '>
+                    
+                    <div className='w-[88px] h-[88px]  rounded-md flex border-2  justify-center items-center  mr-[24px] '>
                         <img src='/product/dres1.png' className='w-[46px] h-[72px]  ' />
                     </div>
-                    <div className='w-[88px] h-[88px]  bg-purple-400 flex border-2 rounded-sm justify-center items-center '>
-                        <img src='/product/dres1.png' className='w-[46px] h-[72px] ' />
+                    
+                    
+                    <div className='w-[88px] h-[88px]  rounded-md flex border-2  justify-center items-center  mr-[24px]'>
+                        <img src='/product/dres1.png' className='w-[46px] h-[72px]  ' />
                     </div>
-                    <div className='w-[88px] h-[88px]  bg-purple-400 flex border-2 rounded-sm justify-center items-center '>
-                        <img src='/product/dres1.png' className='w-[46px] h-[72px] ' />
+                    
+                    
+                    <div className='w-[88px] h-[88px]  rounded-md flex border-2  justify-center items-center  mr-[24px]'>
+                        <img src='/product/dres1.png' className='w-[46px] h-[72px]  ' />
+                    </div>                    
+                    
+                    <div className='w-[88px] h-[88px] rounded-md  flex border-2  justify-center items-center '>
+                        <img src='/product/dres1.png' className='w-[46px] h-[72px]  ' />
                     </div>
-                    <div className='w-[88px] h-[88px]  bg-purple-400 flex border-2 rounded-sm justify-center items-center '>
-                        <img src='/product/dres1.png' className='w-[46px] h-[72px] ' />
-                    </div>
+                    
+                    
+                 
+                   
                   
                     
                 </div>
@@ -36,15 +47,45 @@ function CardComponent() {
         </div>
         <div className='px-[32px] '>
             <div className='flex flex-row justify-between items-center py-[32px]'>
-            <section className='px-[21px] pt-[6px] pb-[8px] text-[14px] text-black bg-slate-500 rounded-full'>
+            <section className='px-[21px] pt-[6px] pb-[8px] text-[14px] text-black bg-neutral-300 rounded-full font-semibold'>
                 Popular
             </section>
-            <h1>heart</h1>
+            
+            <div>
+            <div className=' bg-pink-100 rounded-full p-2  '>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<path d="M11.7058 20.8998L11.7065 20.9003C11.7788 20.9597 11.8823 21 12 21C12.1177 21 12.2215 20.9596 12.2937 20.9004L12.2939 20.9002C13.2713 20.0996 14.2122 19.3485 15.038 18.6895C17.4792 16.7408 19.4882 15.1329 20.8709 13.5701L20.8709 13.57C22.381 11.8633 23 10.3492 23 8.75591C23 7.15508 22.4161 5.70786 21.3836 4.65873C20.3387 3.59726 18.8949 3 17.2956 3C16.1199 3 15.0567 3.3443 14.1114 4.02909L14.1113 4.02913C13.6296 4.37804 13.1869 4.80962 12.7914 5.32162C12.602 5.56682 12.3097 5.71039 11.9999 5.71036C11.6901 5.71033 11.3978 5.5667 11.2085 5.32147C10.8133 4.8097 10.3705 4.37811 9.8888 4.02909L11.7058 20.8998ZM11.7058 20.8998C10.7323 20.1024 9.79427 19.3536 8.97093 18.6964M11.7058 20.8998L8.97093 18.6964M8.97093 18.6964L8.97064 18.6962L8.97093 18.6964ZM2.61663 4.65873C3.66151 3.59725 5.1051 3 6.70459 3C7.88026 3 8.94346 3.34428 9.88873 4.02904L2.61663 4.65873Z" stroke="#EDA3B5" stroke-width="2" stroke-linejoin="round"/>
+</svg>
+                </div>
+            </div>
+            
             </div>
             <h1 className='mt-[24px] text-[48px] font-sans max-w-[348px]'>Black Valentino
 dress with tulle </h1>  
 
-            bgfd
+           <div className='flex flex-row'>           
+
+<div class="flex items-center">
+    <svg class="w-4 h-4 text-[#F4A2B6] ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+    </svg>
+    <svg class="w-4 h-4 text-[#F4A2B6] ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+    </svg>
+    <svg class="w-4 h-4 text-[#F4A2B6] ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+    </svg>
+    <svg class="w-4 h-4 text-[#F4A2B6] ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+    </svg>
+    <svg class="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+    </svg>
+</div>
+    <span className='text-[16px] text-[#1B2437] pl-[16px] items-center justify-center\'>132 reviews</span>
+
+
+           </div>
         </div>
     </div>
     </div>
