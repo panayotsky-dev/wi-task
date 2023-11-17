bbb052f4cc4a85a6a01ea84d74ecec27d74b9936
Panayotsky-dev <panayotsky.dev@gmail.com>
2023-11-17 17:38:30 +0200

    +

--- src/app/components/Reviews/ReviewsComponent.jsx
+++ src/app/components/Reviews/ReviewsComponent.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReviewStats from '../Card/ReviewStats'
+import FormComponent from './FormComponent'
 
 function ReviewsComponent() {
   return (
     <div className='mt-[39px] w-[1140px] h-[928px] bg-white rounded-xl pl-[40px] pr-[38px]'>
         <h2 className='text-[#1B2437] font-sans text-[34px] mt-[56.5px]'>Reviews</h2>
         <ReviewStats />
-        
+        <FormComponent />
     </div>
   )
 }
