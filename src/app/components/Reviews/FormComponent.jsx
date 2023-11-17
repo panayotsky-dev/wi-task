1df4c9787b7a06dfb820b7cb23da14d0a892214f
Panayotsky-dev <panayotsky.dev@gmail.com>
2023-11-17 18:08:53 +0200

    +

--- src/app/components/Reviews/FormComponent.jsx
+++ src/app/components/Reviews/FormComponent.jsx
@@ -8,19 +8,19 @@ function FormComponent() {
         <div className='grid grid-cols-2 gap-x-[38px] justify-start w-full'>
         <div className='items-center'>
         <label for="first-name" class=" text-sm  leading-6 ">Name</label>
-         <input type="text"  placeholder='Type name...' className=" mt-[7px] w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+         <input type="text"  placeholder='Type name...' className=" mt-[7px] w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2  sm:text-sm sm:leading-6" />
          </div>
         
          <div>
         <label for="first-name" class=" text-sm  leading-6 ">Email</label>
-         <input type="email" placeholder='Type email...'  className=" mt-[7px] w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+         <input type="email" placeholder='Type email...'  className=" mt-[7px] w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6" />
          </div>
         </div>
         <div className='mt-[41px] '>
         <label className=' mt-[111px] text-[16px] '>
           Message  
         </label>
-        <input placeholder="Type message..." className=' mt-[7px] w-full    border-0 px-3.5   ring-1 ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6 items-start flex  h-[186px]  rounded-md' />
+        <input placeholder="Type message..." className=' mt-[7px] w-full    border-0 px-3.5   ring-1 ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6 items-start flex  pb-[151px] py-[17px] rounded-md' />
        
         </div>
         <div className=' justify-end flex mt-[32px]'>
---
+++ src/app/utils/data.js
@@ -0,0 +1,40 @@
+export const productData = [
+    {
+        id:1,
+        title:'Black Valentino dress with tulle',
+        price:1315,
+        sizes:[XS,S,M],
+        colors:['#1B2437','#127681','#32E0C4'],
+        info:`Dress with tulle and collar Peter Pan from REDValentino 
+        (Red Valentino). Peter Pan collar, tulle panels, sleeveless model, 
+        concealed back zipper and pleated skirt. Black colour.`,
+        brand:`Brand with tulle and collar Peter Pan from REDValentino 
+        (Red Valentino). Peter Pan collar, tulle panels, sleeveless model, 
+        concealed back zipper and pleated skirt. Black colour.`,
+        delivery:`Delivery with tulle and collar Peter Pan from REDValentino 
+        (Red Valentino). Peter Pan collar, tulle panels, sleeveless model, 
+        concealed back zipper and pleated skirt. Black colour.`,
+        reviews:132,
+        score:4,
+        image:'product/dres1.png',
+        thumbnails:['product/dres1.png','product/dres2.png','product/dres3.png','product/dres4.png','product/dres1.png'],
+        reviews:[
+            {
+                id:1,
+                name:'John Doe',
+                email:'john.doe@gmail.com',
+                rating:4,
+                message:'example review',
+            },
+            {
+                id:2,
+                name:'John Not Doe',
+                email:'john.notdoe@gmail.com',
+                rating:4,
+                message:'Not example review',
+            },
+
+        ]
+    },
+    
+]
